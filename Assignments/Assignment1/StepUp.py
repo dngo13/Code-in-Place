8461765c82199100ceeb1a0f605ce81108d3bf23
Diane Ngo <diane.ngo1321@gmail.com>
2020-04-13 20:47:04 -0400

    Updated with Assignment 1 Problem 1 complete

---
+++ Assignments/Assignment1/StepUp.py
@@ -0,0 +1,29 @@
+# File: StepUp.py
+# This tells PyCharm who Karel is
+# Every Karel file has a line just like it
+from karel.stanfordkarel import *
+
+# this program executes in a special function called main
+def main():
+    # Commands in the run method are executed one at a time
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    # Have Karel turn right
+    turn_right()
+    move()
+    put_beeper()
+    move()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+# This is "boilerplate" code which launches your code
+# when you type "python StepUp.py" in the terminal
+if __name__ == "__main__" :
+    run_karel_program()
+
--- Assignments/Assignment1/TripleKarel.py
+++ Assignments/Assignment1/TripleKarel.py
@@ -2,6 +2,8 @@ from karel.stanfordkarel import *
 
 """
 File: TripleKarel.py
+Diane Ngo
+April 13, 2020
 --------------------
 When you finish writing this file, TripleKarel should be
 able to paint the exterior of three buildings in a given
@@ -10,17 +12,36 @@ should make sure that your program works for all of the
 Triple sample worlds supplied in the starter folder.
 """
 
-
 def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
-    pass
+    for i in range(2):
+        paint_rectangle()
+        turn_around()
+    paint_rectangle()
+    turn_right()
 
+# Defines the turn_right function for Karel
+def turn_right():
+    for i in range(3):
+        turn_left()
 
+# Defines the turn_around function for Karel
+def turn_around():
+    turn_left()
+    turn_left()
+
+# Paints the 3 sides of a rectangle
+def paint_rectangle():
+    for i in range(2):
+        paint_side()
+        move()
+    paint_side()
+
+# If Karel is adjacent to a side of the building, paint
+def paint_side():
+    while left_is_blocked():
+        put_beeper()
+        move()
+    turn_left()
 # There is no need to edit code beyond this point
 
 if __name__ == "__main__":
